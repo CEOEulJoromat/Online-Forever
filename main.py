f1c87d67043014bb0481943ca0064a395e44670e
Mr-MythicMythic <>
2021-12-15 00:30:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+my_secret = os.environ['TOKEN']
+my_secret = os.environ['TOKEN']
+my_secret = os.environ['TOKEN']
+my_secret = os.environ['TOKEN']
+my_secret = os.environ['TOKEN']
+import discord, os, keep_alive, asyncio, datetime, requests
+
+from discord.ext import tasks, commands
+
+client = commands.Bot(
+  command_prefix=':',
+  self_bot=True
+)
+
+async def on_ready():
+    client.remove_command('help')
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("TESTING"))
+
+keep_alive.keep_alive()
+client.run(os.getenv("TOKEN"), bot=False)
